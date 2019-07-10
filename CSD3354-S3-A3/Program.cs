02ba06cd3faa2eb1e931ec79631a2ef0907731eb
Ramesh <ramesh.ramesh@uxplore.ca>
2019-07-10 08:47:48 -0400

    first commit

---
+++ CSD3354-S3-A3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CSD3354_S3_A3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataProcessing dataProcessing = new DataProcessing();
+            dataProcessing.processData();
+        }
+    }
+
+    public class DataProcessing
+    {
+        public void processData()
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+        
+    }
+}

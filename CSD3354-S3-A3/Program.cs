453a7fcc4c6e84e51c5d87f6b585641e303c8b22
Ramesh <ramesh.ramesh@uxplore.ca>
2019-07-10 09:56:45 -0400

    assignment done

--- CSD3354-S3-A3/Program.cs
+++ CSD3354-S3-A3/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
 
 namespace CSD3354_S3_A3
 {
@@ -15,8 +18,56 @@ namespace CSD3354_S3_A3
     {
         public void processData()
         {
-            Console.WriteLine("Hello World!");
-            Console.ReadLine();
+            string path = "C:/C#ClassStuff/July10ReadData/CSD3354-S3-A3/CSD3354-S3-A3/DataContainer.txt";
+            Queue dataQueue = new Queue();
+            Stack dataStack = new Stack();
+            LinkedList<String> dataList = new LinkedList<String>();
+            using (StreamReader sr = File.OpenText(path))
+            {
+                String s = "";
+
+                while ((s = sr.ReadLine()) != null)
+                {
+                    dataQueue.Enqueue(s);
+                    
+                }
+            }
+
+            Console.WriteLine("Printing Data from Queue(FIFO) and entering in stack");
+            foreach (Object obj in dataQueue)
+            {
+
+                dataStack.Push(obj);
+                Console.WriteLine(obj);
+                
+            }
+
+            Console.WriteLine("Printing Data from Stack(LIFO) and entering in LinkedList");
+            foreach (string str in dataStack)
+            {
+                dataList.AddLast(str);
+                Console.WriteLine(str);
+               
+            }
+            Console.WriteLine("Printing Data from LinkedList before sorting");
+            foreach (string str in dataList)
+            {
+                //dataList.AddLast(str);
+                Console.WriteLine(str);
+
+            }
+            var nameList = new List<String>();
+            nameList.AddRange(dataList);
+            nameList.Sort();
+
+            Console.WriteLine("Printing Data from LinkedList after sorting");
+            foreach (string str in nameList)
+            {
+                Console.WriteLine(str);
+
+            }
+
+            Console.ReadKey();
         }
         
     }
